1be31114646c158c35864757a22816fd866babaf
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-08-27 01:21:15 +0600

    woderul sys of fething data

--- app.js
+++ app.js
@@ -9,3 +9,37 @@ function loadData3(){
    .then(res => res.json())
    .then(json => console.log(json))
 }
+
+
+// ************************
+function loadData4(){
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(res => res.json())
+   .then(data => crackData(data))
+};
+
+function crackData(data){
+   for(const user of data){
+      console.log(user);
+   }
+}
+//****************************
+
+
+// ******************************************************
+function loadData8(){
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(res => res.json())
+   .then(data => displayData(data))
+};
+
+function displayData(data){
+   const ul = document.getElementById('user-list');
+   
+   for(const user of data){
+      const li = document.createElement('li');
+      li.innerText = user.name;
+      ul.appendChild(li);
+   }
+}
+// ******************************************************
